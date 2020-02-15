baca6a8e7c4f2f5660eb5892d5c6c9ffeea26313
tom <thomasmaxwellsmith@gmail.com>
2020-02-15 20:07:40 +0000

    simple triangle checkaboard

--- src/modules/simple.ts
+++ src/modules/simple.ts
@@ -1,6 +1,31 @@
+const returnMatrix = (horizontalLength, verticalLength, callback) => {
+  Array(verticalLength)
+    .fill(null)
+    .forEach((_, verticalIndex) => {
+      Array(horizontalLength)
+        .fill(null)
+        .forEach((_, horizontalIndex) => {
+          callback(verticalIndex, horizontalIndex);
+        });
+    });
+};
+
 export default function() {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "red";
-  ctx.fillRect(10, 10, 150, 100);
+  const SIZE = 50;
+  const grid = [10, 10];
+  const drawTriangle = (x: number, y: number) => {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + SIZE, y);
+    ctx.lineTo(x, y + SIZE);
+    ctx.fill();
+    ctx.closePath();
+  };
+
+  ctx.beginPath();
+  returnMatrix(grid[0], grid[1], (verticalIndex, horizontalIndex) => {
+    drawTriangle(verticalIndex * SIZE, horizontalIndex * SIZE);
+  });
 }
